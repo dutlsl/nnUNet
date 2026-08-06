edbab788d9d4cf110a76aee41a2a7a3b7556373f
hyun02051 <hyun02051@naver.com>
2026-08-06 13:14:33 +0900

    docs(report): add official Mamba-3 integration and 192x192 RITnet verification report

--- run_host_sagd.sh
+++ run_host_sagd.sh
@@ -5,8 +5,7 @@ export nnUNet_preprocessed=/home/iulab0/PycharmProjects/nnUNet/nnUNet_preprocess
 export nnUNet_results=/home/iulab0/PycharmProjects/nnUNet/nnUNet_results
 export WANDB_MODE=online
 export CUDA_VISIBLE_DEVICES=0
-export nnUNet_compile=false
 export OPENCV_LOG_LEVEL=OFF
 export OPENCV_VIDEOIO_DEBUG=0
 
-nohup /home/iulab0/PycharmProjects/nnUNet/nnunet_env/bin/python -u /home/iulab0/PycharmProjects/nnUNet/nnunetv2/run/run_training.py Dataset600_OpenEDS2019 2d 1 -tr nnUNetTrainer_Vivim > logs/host_backbone_train.log 2>&1 &
+/home/iulab0/PycharmProjects/nnUNet/nnunet_env/bin/python -u /home/iulab0/PycharmProjects/nnUNet/nnunetv2/run/run_training.py Dataset600_OpenEDS2019 2d 0 -tr nnUNetTrainer_Vivim_SADG
